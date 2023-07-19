75adbdf7435772cf636c6b7c408b62d299995f5d
Changala Siva Rama Krishna Prasad <77905636+SrkPrasadChangala@users.noreply.github.com>
2023-07-19 10:31:30 -0400

    converted code to recursive

--- RecursionAssignment/GeometricSum.java
+++ RecursionAssignment/GeometricSum.java
@@ -3,12 +3,17 @@ public class solution {
 
 	public static double findGeometricSum(int k){
 		// Write your code here
-		double ans = 1;
-		for (int i = 1; i <= k; i++) {
-			double temp = Math.pow(2,i);
-			ans += 1/temp;
+		// double ans = 1;
+		// for (int i = 1; i <= k; i++) {
+		// 	double temp = Math.pow(2,i);
+		// 	ans += 1/temp;
+		// }
+		// return ans;
+		if ( k == 0) {
+			return 1;
 		}
-		return ans;
 
+		double smallans=findGeometricSum(k-1);
+        	return smallans+(1/Math.pow(2,k));
 	}
 }
